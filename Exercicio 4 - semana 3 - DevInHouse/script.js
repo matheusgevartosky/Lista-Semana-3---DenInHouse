b80542719b3940457008b2b37c37c6c2766e5958
matheusgevartosky <magevartosky@gmail.com>
2022-03-17 17:07:52 -0300

    Add exercicio 05 - semana 3

--- Exercicio 4 - semana 3 - DevInHouse/script.js
+++ Exercicio 4 - semana 3 - DevInHouse/script.js
@@ -6,3 +6,23 @@ function mostraDiv(obj) {
 }
 
 
+function keyFunction(event) {
+  let key = event.which;
+  console.log(event.which);
+  if (key == 65) {
+    document.body.style.backgroundColor = "yellow";
+  } else if (key == 80) {
+    document.body.style.backgroundColor = "black";
+  } else if (key == 32) {
+    console.log("apertou espaço");
+    inputElement.style.padding = "20px";
+    inputElement.style.fontSize = "22px";
+  } else if (key == 13) {
+    inputElement.style.removeProperty("padding");
+    inputElement.style.removeProperty("font-size");
+    document.body.style.removeProperty("background-color");
+  
+}}
+
+const inputElement = document.getElementById("input4");
+inputElement.addEventListener("keyup", keyFunction);
