3a885630e96ac77edb54a26e396cc385f7e11e20
matheusgevartosky <magevartosky@gmail.com>
2022-03-17 17:07:30 -0300

    Add Exercicio 5 - semana 3

--- Exercicio 4 - semana 3 - DevInHouse/script.js
+++ Exercicio 4 - semana 3 - DevInHouse/script.js
@@ -2,7 +2,6 @@ function mostraDiv(obj) {
     var el = document.getElementById(obj);
     var visivel = document.getElementsByClassName("show")[0];
     if(visivel && visivel != el) visivel.classList.toggle("show");
-
     el.classList.toggle("show");
 }
 
