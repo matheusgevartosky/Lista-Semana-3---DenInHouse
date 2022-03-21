41c161ac14b064a698943913ec7e8121c07ffd49
Matheus Gevartosky <magevartosky@gmail.com>
2022-03-21 20:08:33 -0300

    Ajusta exercicio 4

--- Exercicio 4 e 5 - semana 3 - DevInHouse/script.js
+++ Exercicio 4 e 5 - semana 3 - DevInHouse/script.js
@@ -1,6 +1,6 @@
 function mostraDiv(obj) {
     var el = document.getElementById(obj);
-    var visivel = document.getElementsByClassName("show");
+    var visivel = document.getElementsByClassName("show")[0];
     if(visivel && visivel != el) visivel.classList.toggle("show");
     el.classList.toggle("show");
 }
