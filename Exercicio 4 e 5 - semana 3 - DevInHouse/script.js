0b3670658fc1d20929ddee5608290f460a917602
Matheus Gevartosky <magevartosky@gmail.com>
2022-03-21 20:02:11 -0300

    Merge

--- Exercicio 4 e 5 - semana 3 - DevInHouse/script.js
+++ Exercicio 4 e 5 - semana 3 - DevInHouse/script.js
@@ -1,6 +1,6 @@
 function mostraDiv(obj) {
     var el = document.getElementById(obj);
-    var visivel = document.getElementsByClassName("show")[0];
+    var visivel = document.getElementsByClassName("show");
     if(visivel && visivel != el) visivel.classList.toggle("show");
     el.classList.toggle("show");
 }
