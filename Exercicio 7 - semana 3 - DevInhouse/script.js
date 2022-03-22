53607385b46bfbfb2da2aac6c319243c835b615f
matheusgevartosky <magevartosky@gmail.com>
2022-03-22 12:36:22 -0300

    Adiciona exercicoio 8

--- Exercicio 7 - semana 3 - DevInhouse/script.js
+++ Exercicio 7 - semana 3 - DevInhouse/script.js
@@ -1,9 +1,19 @@
-const DATA = "https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/BR"
 
-fetch(DATA).then(function(rawData){
-    return rawData.json();
-}).then(function (json){
-    dataManipulation(json);
+var DATA = 'https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/'
+
+console.log(DATA)
+const select = document.getElementById('select')
+select.addEventListener('change', function () {
+    var index = select.selectedIndex
+    console.log
+    var DATA = 'https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/' + (index-1)
+    fetch(DATA).then(function (rawData) {
+        return rawData.json();
+    }).then(function (json) {
+        dataManipulation(json);
+        console.log(json)
+    })
+
 })
 
 function dataManipulation(json) {
@@ -11,6 +21,11 @@ function dataManipulation(json) {
     let container = document.createElement('div')
     let tittle = document.createElement('h1')
     document.body.appendChild(container)
-    tittle.innerHTML = `A população brasileira da data de hoje é de  ${dados} pessoas!`
+    if(json.localidade != 0){
+        tittle.innerHTML = `A população desta região na data de hoje é de  ${dados} pessoas!`
+    }else{
+        tittle.innerHTML = `A população brasileira na data de hoje é de  ${dados} pessoas!`
+    }
+    
     container.appendChild(tittle)
 }
