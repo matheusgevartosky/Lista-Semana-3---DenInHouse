c0abf5ddc856f46000bf9743db8192030b898190
matheusgevartosky <magevartosky@gmail.com>
2022-03-25 17:54:52 -0300

    Ajusta 9

--- Exercicio 9 - Semana 3 - Devinhouse/script.js
+++ Exercicio 9 - Semana 3 - Devinhouse/script.js
@@ -1,3 +1,25 @@
+
+let nomes = ['Matheus', "Ketlin", "Jorge"]
+
+function fetchNome(nome) {
+  const API = "https://servicodados.ibge.gov.br/api/v2/censos/nomes/" + nome;
+
+  fetch(API)
+    .then(function (rawData) {
+      return rawData.json();
+    })
+    .then(function (json) {
+      let returnData = json[0].res;
+      let labelsDinamicas = [];
+      let dadosDinamicos = [];
+
+      for (let index of returnData) {
+        labelsDinamicas.push(index.periodo);
+        dadosDinamicos.push(index.frequencia);
+      }
+      geraGráfico(labelsDinamicas, dadosDinamicos, "matheus ");
+    });
+}
 function geraGráfico(labelsDinamicas, dadosDinamicos, nomeDinamico) {
     const data = {
         labels: labelsDinamicas,
@@ -20,5 +42,11 @@ function geraGráfico(labelsDinamicas, dadosDinamicos, nomeDinamico) {
         config
     );
 }
+
+
+setInterval(function(){
+    
+
+}, 5000;
 
 
