5510c648f177742108c4d327f49910c5f6c5da92
matheusgevartosky <magevartosky@gmail.com>
2022-03-30 15:07:16 -0300

    Exercicio 10 - Semana 3

--- Exercicio 10 - Semana 3 - Devinhouse/script.js
+++ Exercicio 10 - Semana 3 - Devinhouse/script.js
@@ -1,34 +1,38 @@
-const dados = [];
+var controlaLoop = 0;
+const data = localStorage.getItem("data");
+let dados = [];
+
+if (data != null) {
+  const returnData = JSON.parse(data);
+  dados = returnData;
+  startScript()
+}
 
 
+function startScript() {
+  console.log(dados[controlaLoop]);
+  document.body.style.backgroundColor = dados[controlaLoop].corDeFundo;
+  setTimeout(function () {
+    controlaLoop++;
+    if (controlaLoop == dados.length) {
+      controlaLoop = 0
+      startScript()
+    } else {
+      startScript();
+    }
+  }, dados[controlaLoop].timer * 1000);
+}
 
-document.getElementById("dataEntry").addEventListener("submit", (event) => {
-  event.preventDefault();
+let btnIncluir = document.getElementById("dataEntry").addEventListener("submit", (event) => {
   let colors = document.getElementById("color").value;
 
-  let timer = parent(document.getElementById("number").value);
+  let timer = parseInt(document.getElementById("number").value);
 
   const data = {
     corDeFundo: colors,
     timer: timer,
   };
   dados.push(data);
-    const dataToString = JSON.stringify(dados)
-    localStorage.setItem('data', dataToString)
+  const dataToString = JSON.stringify(dados);
+  localStorage.setItem("data", dataToString);
 });
-
-
-const getData = localStorage.getItem('data')
-if(getData != null) {
-    var obj = JSON.parse(getData)
-    console.log(obj)
-}
-
-function changeColor(color){
-    document.body.style.background = color;
-}
-
-setInterval(() => {
-    changeColor(obj[0].corDeFundo)
-}, obj[0].timer);
-
